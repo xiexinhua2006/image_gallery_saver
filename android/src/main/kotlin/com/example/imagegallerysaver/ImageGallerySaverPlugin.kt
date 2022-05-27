2a2049a5c236214660b5c6c5b38e8e3d67cf5cf7
Dr_Black <xiexinhua2006@126.com>
2022-05-28 03:07:48 +0800

    Debug

--- android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
+++ android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
@@ -83,6 +83,7 @@ class ImageGallerySaverPlugin : FlutterPlugin, MethodCallHandler {
             return applicationContext?.contentResolver?.insert(uri, values)!!
         } else {
             val storePath = Environment.getExternalStorageDirectory().absolutePath + File.separator + Environment.DIRECTORY_PICTURES
+            print storePath
             val appDir = File(storePath)
             if (!appDir.exists()) {
                 appDir.mkdir()
