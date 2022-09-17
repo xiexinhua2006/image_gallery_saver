fedcc9e45edfcb777e4534a7feb194cd48d2edea
Dr_Black <xiexinhua2006@126.com>
2022-09-17 23:12:45 +0800

    Update ImageGallerySaverPlugin.kt

--- android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
+++ android/src/main/kotlin/com/example/imagegallerysaver/ImageGallerySaverPlugin.kt
@@ -129,6 +129,7 @@ class ImageGallerySaverPlugin : FlutterPlugin, MethodCallHandler {
                 saveToKoi(bmp, quality, name)
             }
         }
+        SaveResultModel(true, null, null).toHashMap()
     }
 
     private fun saveToKoi(bmp: Bitmap, quality: Int, name: String?): HashMap<String, Any?>{
